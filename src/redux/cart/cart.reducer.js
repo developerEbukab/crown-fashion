d684bef594d86e05960c52505b57454ca9c01f26
ChukwuEbuka Beluolisa <developerebukab@gmail.com>
2020-12-28 15:07:34 -0500

    Added add item and remove item functionality

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import { CartActionTypes } from "./cart.types";
-import { addItemToCart } from "./cart.utils";
+import { addItemToCart, clearItemFromCart, removeItemFromCart } from "./cart.utils";
 
 const INITIAL_STATE = {
   hidden: true,
@@ -15,10 +15,21 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       }
     
     case CartActionTypes.ADD_ITEM:
-      console.log("HERE IS THE ITEM ", action.payload)
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload)
+      }
+    
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
+      }
+    
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: clearItemFromCart(state.cartItems, action.payload)
       }
     
     default:
