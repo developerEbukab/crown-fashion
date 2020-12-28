0cd19263f36697c4c47c5f101dda1a0d3279a464
ChukwuEbuka Beluolisa <developerebukab@gmail.com>
2020-12-28 11:22:50 -0500

    Added and implemented the reselect library

--- src/components/Cart-Dropdown/Cart-Dropdown.component.jsx
+++ src/components/Cart-Dropdown/Cart-Dropdown.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 import CartItem from '../Cart-Item/Cart-Item.component';
 import CustomButton from '../Custom-Button/Custom-Button.component';
 import "./Cart-Dropdown.styles.scss";
@@ -17,8 +18,8 @@ const CartDropdown = ({cartItems}) => {
   );
 };
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-  cartItems
+const mapStateToProps = (state) => ({
+  cartItems : selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropdown);
